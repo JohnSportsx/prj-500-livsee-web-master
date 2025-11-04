c6c3a0a83f560efbc6c5d0ef1ad68d417669e49d
infoe2logy <itsupport@e2logy.com>
2025-11-04 17:58:45 +0530

    ...

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,21 @@ import type { NextConfig } from "next";
 
 const isDev = process.env.NEXT_PUBLIC_NODE_ENV === "dev";
 
-
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prj-500-livsee-web-master.vercel.app",
+        port: "",
+        pathname: "/images/**", // Adjust pathname if necessary, but /images/** is common
+      },
+    ],
   },
-  basePath: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH || '/livsee',
-  assetPrefix: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH || '/livsee',
+  basePath: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH || "/livsee",
+  assetPrefix: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH || "/livsee",
 };
 
 export default nextConfig;
-
